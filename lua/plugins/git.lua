4f24cf94f4f94d86d01ecdcd0142262e6677ed33
ROMANVIKI <vikiadhi2016@gmail.com>
2025-07-19 09:03:21 +0530

    added some useful packages

---
+++ lua/plugins/git.lua
@@ -0,0 +1,43 @@
+-- Complete Git setup
+return {
+  -- Essential Git signs and blame
+  {
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      signs = {
+        add = { text = "+" },
+        change = { text = "~" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+      },
+    },
+  },
+
+  -- LazyGit integration
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = "LazyGit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
+
+  -- Diff viewer
+  {
+    "sindrets/diffview.nvim",
+    dependencies = "nvim-lua/plenary.nvim",
+    cmd = {
+      "DiffviewOpen",
+      "DiffviewFileHistory",
+    },
+    keys = {
+      { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "DiffView" },
+      { "<leader>gh", "<cmd>DiffviewFileHistory<cr>", desc = "File History" },
+    },
+  },
+}
--- lua/plugins/language-support.lua
+++ lua/plugins/language-support.lua
@@ -25,100 +25,6 @@ return {
     },
   },
 
-  -- LSP Configuration
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        -- HTML
-        html = {
-          filetypes = { "html", "htmldjango" },
-        },
-
-        -- CSS
-        cssls = {
-          filetypes = { "css", "scss", "less" },
-          settings = {
-            css = {
-              validate = true,
-              lint = {
-                unknownAtRules = "ignore",
-              },
-            },
-            scss = {
-              validate = true,
-              lint = {
-                unknownAtRules = "ignore",
-              },
-            },
-          },
-        },
-
-        -- JavaScript/TypeScript
-        tsserver = {
-          filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-          settings = {
-            typescript = {
-              preferences = {
-                includePackageJsonAutoImports = "auto",
-              },
-            },
-            javascript = {
-              preferences = {
-                includePackageJsonAutoImports = "auto",
-              },
-            },
-          },
-        },
-
-        -- Python
-        pyright = {
-          settings = {
-            python = {
-              analysis = {
-                typeCheckingMode = "basic",
-                autoSearchPaths = true,
-                useLibraryCodeForTypes = true,
-              },
-            },
-          },
-        },
-
-        -- Tailwind CSS
-        tailwindcss = {
-          filetypes = {
-            "html",
-            "css",
-            "javascript",
-            "javascriptreact",
-            "typescript",
-            "typescriptreact",
-            "vue",
-            "svelte",
-          },
-          settings = {
-            tailwindCSS = {
-              experimental = {
-                classRegex = {
-                  "tw`([^`]*)",
-                  'tw="([^"]*)',
-                  'tw={"([^"}]*)',
-                  "tw\\.\\w+`([^`]*)",
-                  "tw\\(.*?\\)`([^`]*)",
-                },
-              },
-            },
-          },
-        },
-
-        -- Emmet for HTML/CSS
-        emmet_ls = {
-          filetypes = { "html", "css", "scss", "javascript", "javascriptreact", "typescript", "typescriptreact" },
-        },
-      },
-    },
-  },
-
   -- Enhanced JSX/TSX support
   {
     "windwp/nvim-ts-autotag",
@@ -133,93 +39,63 @@ return {
   },
 
   -- Better JavaScript/TypeScript experience
-  {
-    "pmizio/typescript-tools.nvim",
-    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    ft = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-    opts = {
-      settings = {
-        separate_diagnostic_server = true,
-        publish_diagnostic_on = "insert_leave",
-        expose_as_code_action = {},
-        tsserver_path = nil,
-        tsserver_plugins = {},
-        tsserver_max_memory = "auto",
-        tsserver_format_options = {},
-        tsserver_file_preferences = {},
-        tsserver_locale = "en",
-        complete_function_calls = false,
-        include_completions_with_insert_text = true,
-        code_lens = "off",
-        disable_member_code_lens = true,
-        jsx_close_tag = {
-          enable = false,
-          filetypes = { "javascriptreact", "typescriptreact" },
-        },
-      },
-    },
-  },
+  -- {
+  --   "pmizio/typescript-tools.nvim",
+  --   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+  --   ft = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+  --   opts = {
+  --     settings = {
+  --       separate_diagnostic_server = true,
+  --       publish_diagnostic_on = "insert_leave",
+  --       expose_as_code_action = {},
+  --       tsserver_path = nil,
+  --       tsserver_plugins = {},
+  --       tsserver_max_memory = "auto",
+  --       tsserver_format_options = {},
+  --       tsserver_file_preferences = {},
+  --       tsserver_locale = "en",
+  --       complete_function_calls = false,
+  --       include_completions_with_insert_text = true,
+  --       code_lens = "off",
+  --       disable_member_code_lens = true,
+  --       jsx_close_tag = {
+  --         enable = false,
+  --         filetypes = { "javascriptreact", "typescriptreact" },
+  --       },
+  --     },
+  --   },
+  -- },
 
   -- Python-specific enhancements
-  {
-    "linux-cultist/venv-selector.nvim",
-    dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim" },
-    ft = "python",
-    opts = {
-      name = "venv",
-      auto_refresh = false,
-    },
-    event = "VeryLazy",
-    keys = {
-      { "<leader>vs", "<cmd>VenvSelect<cr>" },
-      { "<leader>vc", "<cmd>VenvSelectCached<cr>" },
-    },
-  },
 
   -- CSS/SCSS enhancements
-  {
-    "NvChad/nvim-colorizer.lua",
-    event = "LazyFile",
-    opts = {
-      filetypes = { "css", "scss", "html", "javascript", "javascriptreact", "typescript", "typescriptreact" },
-      user_default_options = {
-        RGB = true,
-        RRGGBB = true,
-        names = true,
-        RRGGBBAA = false,
-        AARRGGBB = true,
-        rgb_fn = false,
-        hsl_fn = false,
-        css = false,
-        css_fn = false,
-        mode = "background",
-        tailwind = true,
-        sass = { enable = true, parsers = { "css" } },
-        virtualtext = "■",
-      },
-    },
-  },
+  -- {
+  --   "NvChad/nvim-colorizer.lua",
+  --   event = "LazyFile",
+  --   opts = {
+  --     filetypes = { "css", "scss", "html", "javascript", "javascriptreact", "typescript", "typescriptreact" },
+  --     user_default_options = {
+  --       RGB = true,
+  --       RRGGBB = true,
+  --       names = true,
+  --       RRGGBBAA = false,
+  --       AARRGGBB = true,
+  --       rgb_fn = false,
+  --       hsl_fn = false,
+  --       css = false,
+  --       css_fn = false,
+  --       mode = "background",
+  --       tailwind = true,
+  --       sass = { enable = true, parsers = { "css" } },
+  --       virtualtext = "■",
+  --     },
+  --   },
+  -- },
 
   -- HTML/XML tag matching (duplicate removed)
   -- Note: nvim-ts-autotag was already defined above
 
   -- Better folding for all languages
-  {
-    "kevinhwang91/nvim-ufo",
-    dependencies = "kevinhwang91/promise-async",
-    event = "LazyFile",
-    opts = {
-      provider_selector = function(bufnr, filetype, buftype)
-        return { "treesitter", "indent" }
-      end,
-    },
-    init = function()
-      vim.o.foldcolumn = "1"
-      vim.o.foldlevel = 99
-      vim.o.foldlevelstart = 99
-      vim.o.foldenable = true
-    end,
-  },
 
   -- Tailwind CSS tools
   {
@@ -227,41 +103,6 @@ return {
     config = function()
       require("tailwindcss-colorizer-cmp").setup({
         color_square_width = 2,
-      })
-    end,
-  },
-
-  -- REST client for API testing (useful for JS/Python projects)
-  {
-    "rest-nvim/rest.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    ft = "http",
-    config = function()
-      require("rest-nvim").setup({
-        result_split_horizontal = false,
-        result_split_in_place = false,
-        skip_ssl_verification = false,
-        encode_url = true,
-        highlight = {
-          enabled = true,
-          timeout = 150,
-        },
-        result = {
-          show_url = true,
-          show_curl_command = false,
-          show_http_info = true,
-          show_headers = true,
-          formatters = {
-            json = "jq",
-            html = function(body)
-              return vim.fn.system({ "tidy", "-i", "-q", "-" }, body)
-            end,
-          },
-        },
-        jump_to_request = false,
-        env_file = ".env",
-        custom_dynamic_variables = {},
-        yank_dry_run = true,
       })
     end,
   },
